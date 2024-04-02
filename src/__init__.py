ffcf0fb358b99320e7c725d86dbcce8f235926c9
Mark Nelson <mark.a.nelson@outlook.com>
2024-04-02 22:23:15 +0000

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+#! /usr/local/bin/python3
+# -*- mode: python ; coding: utf-8 -*-
+
+from sasci360apicore import communication
+from sasci360apicore import connection
+from sasci360apicore import data
+from sasci360apicore import encryption
+from sasci360apicore import listener
+from sasci360apicore import logger
+from sasci360apicore import reporter
+from sasci360apicore import scheduler
+
+from sasci360apidigitalassets import base
+from sasci360apidigitalassets import digital_assets
+from sasci360apidigitalassets import folders
+from sasci360apidigitalassets import jobs
+from sasci360apidigitalassets import properties_file
+from sasci360apidigitalassets import renditions
+from sasci360apidigitalassets import revisions
+from sasci360apidigitalassets import root
